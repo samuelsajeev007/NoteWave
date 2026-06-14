f10e4dbb0ad45334c2821f21a6798ea605d07959
samuelsajeev007 <samuelsajeev6874@gmail.com>
2026-06-14 22:56:34 +0530

    Initial Commit

---
+++ notewave/notewaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  notewaveApp.swift
+//  notewave
+//
+//  Created by Samuel Sajeev on 14/06/26.
+//
+
+import SwiftUI
+
+@main
+struct notewaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
